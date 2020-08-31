aa73c11a6088c8c0e3f6d6c419104b5c538e96cf
Miguel Rodriguez <ultralord239@gmail.com>
2020-08-31 18:05:39 +0900

    added some responsive design to the logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,9 @@ import NavigationItems from '../NavigationItems/NavigationsItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
+        <div className={classes.Logo}>
         <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
